57ebd8fb8b6bf067d6f19dfa5fd1c74ae48951d8
Rob Ranf <rob@emiyaconsulting.com>
2021-05-15 14:12:24 -0700

    adding winston for error logging

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, printf } = format;
 const connectDB = require("./config/db");
 require("colors");
 
@@ -16,7 +18,35 @@ connectDB();
 // Route imports
 const bootcamps = require("./routes/bootcamps");
 
-// Log stream
+// Logger for errors and info
+const logFormat = printf(({ level, message, label, timestamp }) => {
+	return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+	format: combine(label({ label: "server.js" }), timestamp(), logFormat),
+	transports: [
+		// - Write all logs with level "error" and below to "error.log"
+		// - Write all logs with level "info" and below to "combined.log"
+		new transports.File({
+			filename: path.join(__dirname + "/logs", "error.log"),
+			level: "error",
+		}),
+		new transports.File({
+			filename: path.join(__dirname + "/logs", "combined.log"),
+		}),
+	],
+});
+
+if (process.env.NODE_ENV !== "production") {
+	logger.add(
+		new transports.Console({
+			format: format.simple(),
+		})
+	);
+}
+
+// Log stream for http requests with dev logging middleware
 var accessLogStream = fs.createWriteStream(
 	path.join(__dirname + "/logs", "access.log"),
 	{
@@ -24,7 +54,6 @@ var accessLogStream = fs.createWriteStream(
 	}
 );
 
-// Dev logging middleware
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("combined", { stream: accessLogStream }));
 }
@@ -42,6 +71,10 @@ const server = app.listen(process.env.PORT || 3000, () => {
 // Handle unhandled promise rejections by crashing the app
 process.on("unhandledRejection", (err) => {
 	console.log(`Error: ${err.message}`.red.underline.bold);
+	logger.log({
+		level: "error",
+		message: `error - unhandled rejection: ${err.message}`,
+	});
 	// Close server and exit process
 	server.close(() => {
 		process.exit(1);
