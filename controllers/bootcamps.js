c2376daaaf0f018582418d8ce2d0b6cd908f96ad
Rob Ranf <rob@emiyaconsulting.com>
2021-05-21 17:01:53 -0700

    adding errors for duplicate keys and validation failures

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
-const ErrorResponse = require("../utils/errorResponse");
 const Bootcamp = require("../models/Bootcamp");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -13,11 +13,7 @@ exports.getBootcamps = async (req, res, next) => {
 			data: bootcamps,
 		});
 	} catch (err) {
-		next(
-			new ErrorResponse(
-				"Error in /controllers/bootcamps.js getBootcamps(): Error retrieving bootcamps"
-			)
-		);
+		next(err);
 	}
 };
 
@@ -28,22 +24,19 @@ exports.getBootcamp = async (req, res, next) => {
 	try {
 		const bootcamp = await Bootcamp.findById(req.params.id);
 		if (!bootcamp) {
-			return res.status(404).json({
-				success: false,
-				msg: `No resource with the id of ${req.params.id}`,
-			});
+			return next(
+				new ErrorResponse(
+					`No resource found with the id of ${req.params.id}`,
+					404
+				)
+			);
 		}
 		return res.status(200).json({
 			success: true,
 			data: bootcamp,
 		});
 	} catch (err) {
-		next(
-			new ErrorResponse(
-				`Error in /controllers/bootcamps.js getBootcamp(): ${req.params.id} is not a valid bootcamp id`,
-				400
-			)
-		);
+		next(err);
 	}
 };
 
@@ -74,10 +67,10 @@ exports.updateBootcamp = async (req, res, next) => {
 			{ new: true, runValidators: true }
 		);
 		if (!bootcamp) {
-			return res.status(404).json({
-				success: false,
-				message: `No resource with the id of ${req.params.id}`,
-			});
+			new ErrorResponse(
+				`No resource found with the id of ${req.params.id}`,
+				404
+			);
 		}
 		return res.status(200).json({
 			success: true,
@@ -104,10 +97,10 @@ exports.deleteBootcamp = async (req, res, next) => {
 			}
 		);
 		if (!bootcamp) {
-			return res.status(404).json({
-				success: false,
-				message: `No resource with the id of ${req.params.id}`,
-			});
+			new ErrorResponse(
+				`No resource found with the id of ${req.params.id}`,
+				404
+			);
 		}
 		return res.status(200).json({
 			success: true,
--- middleware/error.js
+++ middleware/error.js
@@ -1,18 +1,42 @@
 const logger = require("../middleware/errorLogger");
+const ErrorResponse = require("../utils/errorResponse");
 
 /* eslint-disable-next-line */
 const errorHandler = (err, req, res, next) => {
+	let error = {
+		...err,
+	};
+	error.message = err.message;
 	// Log to console for dev
-	console.log(err.stack.red);
+	console.log(err);
 
-	res.status(err.statusCode || 500).json({
+	// Mongoose bad ObjectId
+	if (err.name === "CastError") {
+		const message = `${err.name} : ${err.message} : ${err.value} is not a valid ${err.kind} at ${err.path} : ${err.reason}`;
+		error = new ErrorResponse(message, 400);
+	}
+
+	// Mongoose duplicate key
+	if (err.code === 11000) {
+		const message = `${err.name} : ${err.message} : "${err.keyValue.name}" already exists in the database`;
+		error = new ErrorResponse(message, 400);
+	}
+
+	// Mongoose validation failure
+	if (err.name === "ValidationError") {
+		const subMessage = Object.values(err.errors).map((val) => val.message);
+		const message = `${err.name} - ${err._message} : ${subMessage}`;
+		error = new ErrorResponse(message, 400);
+	}
+
+	res.status(error.statusCode || 500).json({
 		success: false,
-		error: err.message || "Server Error",
+		error: error.message || "Server Error",
 	});
 
 	logger.log({
 		level: "error",
-		message: `${err.message}`,
+		message: `${error.message}`,
 	});
 };
 
