946db98bfb06808ca00d67e6bef6e5f7f2e8c76a
Rob Ranf <rob@emiyaconsulting.com>
2021-06-14 16:03:20 -0700

    adding course cost aggregation

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const ErrorResponse = require("../utils/errorResponse");
 
 const CourseSchema = new mongoose.Schema(
 	{
@@ -42,5 +43,43 @@ const CourseSchema = new mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+// Static method to calculate average of course tuitions; on Mongoose we can use statics to create static methods which,
+// as with all languages, are methods that do not need to be instantiated to call, i.e. we do not need to save our method into
+// a variable and instantiate the calling object in order to use it as below
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+	const setAverageCost = await this.aggregate([
+		{
+			$match: { bootcamp: bootcampId },
+		},
+		{
+			$group: {
+				_id: "$bootcamp",
+				averageCost: { $avg: "$tuition" },
+			},
+		},
+	]);
+	try {
+		await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+			averageCost: Math.ceil(setAverageCost[0].averageCost / 10) * 10,
+		});
+	} catch (err) {
+		return new ErrorResponse(
+			`Error calculating the average cost of tuition for this bootcamp: ${err}`,
+			400
+		);
+	}
+};
+
+// Call getAverageCost after save; because these are being called on the model (which is basically a class), we need to use a constructor method
+// in order to create and initialize any objects of the Model
+CourseSchema.post("save", function () {
+	this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.pre("remove", function () {
+	this.constructor.getAverageCost(this.bootcamp);
+});
 
 module.exports = mongoose.model("Course", CourseSchema);
