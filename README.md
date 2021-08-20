a2ba0e20042eb759215fcfdc22e169b05ed68ed0
Rob Ranf <rob@emiyaconsulting.com>
2021-08-20 16:19:26 -0700

    removing untracked dummy data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DevCamper project API
+
+Deployment:
+
+1. Copy your env variables into config/config.env once deplyed
---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+/**
+ * @todo //TODO When deploying to prod, update Mongo DB access to allow access from app IP
+ */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rteov.mongodb.net/devcamper?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+	const conn = await mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	});
+	console.log(
+		`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold
+	);
+};
+
+module.exports = connectDB;
